687275e9d2d2dfb84c4c8240dd0937733715d6c7
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2025-05-14 15:16:13 +0200

    handle spyOn on http call

--- src/who-wants-to-be-millionaire/core-logic/usecases/answer-submission/submitAnswer.spec.ts
+++ src/who-wants-to-be-millionaire/core-logic/usecases/answer-submission/submitAnswer.spec.ts
@@ -1,21 +1,47 @@
 import { Pyramid, pyramidFactory } from './pyramidFactory';
-import { beforeEach } from 'vitest';
-import { submitAnswer } from './submitAnswer';
+import { beforeEach, MockInstance, vi } from 'vitest';
+import { SubmitAnswer } from './submitAnswer';
+import { HttpClient } from '@angular/common/http';
 
 describe('Answer submission Use Case', () => {
   let pyramid: Pyramid;
+  let httpClientMock: MockInstance;
 
   beforeEach(() => {
     pyramid = pyramidFactory();
     pyramid.reachedStepIndex = 0;
-  });
-
-  it('After submitting a right answer, the pyramid should increase step', () => {
-    submitAnswer('A', pyramid);
-    expect(pyramid.reachedStepIndex).toBe(1);
+    httpClientMock = vi.fn();
   });
 
   it('Before submitting an answer, the pyramid should be reset', () => {
     expect(pyramid.reachedStepIndex).toBe(0);
   });
+
+  it('After submitting a right answer, the pyramid should increase step', () => {
+    simulateAnswerSubmissionAPI(true);
+    submitAnswer('A');
+    expect(pyramid.reachedStepIndex).toBe(1);
+  });
+
+  it('After submitting a wrong answer, the pyramid should reset', () => {
+    simulateAnswerSubmissionAPI(false);
+    submitAnswer('A');
+    expect(pyramid.reachedStepIndex).toBe(0);
+  });
+
+  const submitAnswer = (givenAnswer: string) => {
+    new SubmitAnswer(pyramid, httpClientMock as unknown as HttpClient).execute(
+      givenAnswer,
+    );
+  };
+
+  const simulateAnswerSubmissionAPI = (shouldBeRightAnswer: boolean) => {
+    httpClientMock.mockImplementation(() => ({
+      post: () => ({
+        subscribe: (callback: (response: boolean) => void) => {
+          callback(shouldBeRightAnswer); // Simulate a right answer
+        },
+      }),
+    }));
+  };
 });
--- src/who-wants-to-be-millionaire/core-logic/usecases/answer-submission/submitAnswer.ts
+++ src/who-wants-to-be-millionaire/core-logic/usecases/answer-submission/submitAnswer.ts
@@ -1,5 +1,24 @@
 import { Pyramid } from './pyramidFactory';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
-export const submitAnswer = (answer: string, pyramid: Pyramid) => {
-  pyramid.reachedStepIndex++;
-};
+export class SubmitAnswer {
+  constructor(
+    private readonly pyramid: Pyramid,
+    private readonly httpClient: HttpClient,
+  ) {}
+
+  async execute(givenAnswer: string): Promise<void> {
+    const isRightAnswer = await lastValueFrom(
+      this.httpClient.post<boolean>('https://api.example.com/validate-answer', {
+        answer: givenAnswer,
+      }),
+    );
+    console.log(
+      this.httpClient.post<boolean>('https://api.example.com/validate-answer', {
+        answer: givenAnswer,
+      }),
+    );
+    if (isRightAnswer) this.pyramid.reachedStepIndex++;
+  }
+}
